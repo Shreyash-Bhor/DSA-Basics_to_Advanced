37b5dcb8a440ecad4218585d42634b8ecd53f576
Shreyash Bhor <shreyeshbhor@gmail.com>
2026-03-15 15:45:46 +0530

    Arrays Q1

---
+++ Arrays/LargestElement.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.*;
+
+public class LargestElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+        System.out.println(largest);
+        sc.close();
+    }
+}
