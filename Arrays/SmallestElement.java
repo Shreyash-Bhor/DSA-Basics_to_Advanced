39943751126afb1e938c0f247044b45c9547304e
Shreyash Bhor <shreyeshbhor@gmail.com>
2026-03-16 09:30:54 +0530

    Arrays Q4

---
+++ Arrays/SmallestElement.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.util.*;
+
+public class SmallestElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int smallest = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+        System.out.println(smallest);
+        sc.close();
+    }
+}
