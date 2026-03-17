f26de2733900890f609935ec431def56cecd0faf
Shreyash Bhor <shreyeshbhor@gmail.com>
2026-03-17 09:24:38 +0530

    Strings Q3

---
+++ Strings/CountVowelsConsonants.java
@@ -0,0 +1,26 @@
+package Strings;
+
+import java.util.*;
+
+public class CountVowelsConsonants {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int vowels = 0;
+        int consonants = 0;
+        s = s.toLowerCase();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        sc.close();
+    }
+}
