4aa5965c6cc3de78778eb846a1efd1fb08e65b33
Shreyash Bhor <shreyeshbhor@gmail.com>
2026-03-25 16:15:34 +0530

    Fundamentals Array Q4

---
+++ Fundamentals/Arrays/Frequency.java
@@ -0,0 +1,24 @@
+package Fundamentals.Arrays;
+
+import java.util.*;
+
+public class Frequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] freq = new int[100];
+        for (int i = 0; i < n; i++) {
+            freq[arr[i]]++;
+        }
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] > 0) {
+                System.out.println(i + " -> " + freq[i]);
+            }
+        }
+        sc.close();
+    }
+}
