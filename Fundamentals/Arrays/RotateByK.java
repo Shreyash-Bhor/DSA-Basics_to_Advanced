be8ebc77279907e490acde2fac0da512cc056750
Shreyash Bhor <shreyeshbhor@gmail.com>
2026-03-28 18:37:14 +0530

    Fundamentals Array Q6

---
+++ Fundamentals/Arrays/RotateByK.java
@@ -0,0 +1,54 @@
+package Fundamentals.Arrays;
+
+import java.util.*;
+
+public class RotateByK {
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void leftRotate(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+
+    public static void rightRotate(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+        reverse(arr, 0, n - k - 1);
+        reverse(arr, n - k, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        leftRotate(arr, k);
+        System.out.println("Left Rotate: ");
+        for (int num : arr)
+            System.out.print(num + " ");
+
+        System.out.println();
+        rightRotate(arr, k);
+        System.out.println("Right Rotate: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        sc.close();
+    }
+}
