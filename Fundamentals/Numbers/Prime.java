7b2d0bdf7612d71521ba703c9fb5c549c8ff739e
Shreyash Bhor <shreyeshbhor@gmail.com>
2026-03-30 17:30:31 +0530

    Fundamentals Number Q1

---
+++ Fundamentals/Numbers/Prime.java
@@ -0,0 +1,23 @@
+package Fundamentals.Numbers;
+
+import java.util.*;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        if (n <= 1) {
+            System.out.println("Not Prime");
+            return;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                System.out.println("Not Prime");
+                return;
+            }
+        }
+        System.out.println("Prime");
+    }
+}
