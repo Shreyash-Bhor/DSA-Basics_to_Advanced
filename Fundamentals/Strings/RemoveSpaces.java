e60d2e4c084b96a0f8b7d69f3bd5f43d257cdf32
Shreyash Bhor <shreyeshbhor@gmail.com>
2026-03-30 14:30:21 +0530

    Fundamentals String Q5

---
+++ Fundamentals/Strings/RemoveSpaces.java
@@ -0,0 +1,20 @@
+package Fundamentals.Strings;
+
+import java.util.*;
+
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        String str = sc.nextLine();
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') {
+                result += str.charAt(i);
+            }
+        }
+        System.out.println(result);
+        sc.close();
+    }
+}
