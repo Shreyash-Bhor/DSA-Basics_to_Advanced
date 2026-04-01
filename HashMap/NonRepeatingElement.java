fb81bd9780061ad5b06a99f3ffa8b68262275516
Shreyash Bhor <shreyeshbhor@gmail.com>
2026-04-01 14:42:16 +0530

    HashMap Q6

---
+++ HashMap/NonRepeatingElement.java
@@ -0,0 +1,25 @@
+package HashMap;
+
+import java.util.*;
+
+public class NonRepeatingElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        sc.close();
+        for (int i = 0; i < n; i++) {
+            if (map.get(arr[i]) == 1) {
+                System.out.println(arr[i]);
+                return;
+            }
+        }
+        System.out.println("No Non - Repeating Element");
+    }
+}
