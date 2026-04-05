b677cd863817f9280c02a4416c20ab70a1c8d47e
Shreyash Bhor <shreyeshbhor@gmail.com>
2026-04-06 02:04:39 +0530

    SlidingWindow Q5

---
+++ SlidingWindow/DistinctElements.java
@@ -0,0 +1,34 @@
+package SlidingWindow;
+
+import java.util.*;
+
+public class DistinctElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int k = sc.nextInt();
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < k; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        System.out.print(map.size() + " ");
+
+        for (int i = k; i < n; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            map.put(arr[i - k], map.get(arr[i - k]) - 1);
+            if (map.get(arr[i - k]) == 0) {
+                map.remove(arr[i - k]);
+            }
+            System.out.print(map.size() + " ");
+        }
+        sc.close();
+    }
+}
