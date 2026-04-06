6224c5726479d73198c0b6a9d3eb4c8c29146ba8
Shreyash Bhor <shreyeshbhor@gmail.com>
2026-04-06 23:12:00 +0530

    Stack Q2

---
+++ Stack/NextGreaterElement.java
@@ -0,0 +1,31 @@
+package Stack;
+
+import java.util.*;
+
+public class NextGreaterElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                result[i] = -1;
+            } else {
+                result[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(result[i] + " ");
+        }
+        sc.close();
+    }
+}
