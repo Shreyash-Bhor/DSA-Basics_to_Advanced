bae6adc99df36f713108f1af29b9d946204f8061
Shreyash Bhor <shreyeshbhor@gmail.com>
2026-04-06 07:11:52 +0530

    SlidingWindow Q6

---
+++ SlidingWindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,25 @@
+package SlidingWindow;
+
+import java.util.*;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        HashSet<Character> set = new HashSet<>();
+
+        int start = 0;
+        int maxLength = 0;
+
+        for (int end = 0; end < s.length(); end++) {
+            while (set.contains(s.charAt(end))) {
+                set.remove(s.charAt(start));
+                start++;
+            }
+            set.add(s.charAt(end));
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+        System.out.println(maxLength);
+        sc.close();
+    }
+}
